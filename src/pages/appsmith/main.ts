486fb3959c6216cc629bd9746c445834e2bb88d7
ichik <vadim@vaitenko.com>
2025-02-08 10:15:33 +0100

    We're almost to the interesting part, almost

--- src/pages/appsmith/main.ts
+++ src/pages/appsmith/main.ts
@@ -1,4 +1,10 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
+// Get the root element
+var r = document.documentElement;
+
+// Create a function for setting a variable value
+function doTheThing() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--blue', 'hotpink');
 }
-hello('TypeScript');
+
+doTheThing();
